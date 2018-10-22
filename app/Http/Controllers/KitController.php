2241d77f4b6de6cc21fbeaa493b56109ff47c3e5
drperalta <peralta.135816@novaliches.sti.ph>
2018-10-22 20:34:05 +0800

    added function add in kit controller

--- app/Http/Controllers/KitController.php
+++ app/Http/Controllers/KitController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Auth;
+use App\Kit;
+use Carbon\Carbon;
 
 class KitController extends Controller
 {
-    //
+    public function add(Request $request){
+
+        $request->validate([
+            'serial_number' => 'required|unique:kits'
+        ]);
+
+        $kit = new Kit([
+            'serial_number' => $request['serial_number'],
+            'created_at' => Carbon::now()->setTimezone('GMT+8'),
+            'updated_at' => Carbon::now()->setTimezone('GMT+8')
+        ]);
+        $kit->save();
+
+        return response()->json([
+            'message' => 'Successfully Added!'
+        ], 201);
+
+    }
 }
