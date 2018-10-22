8e8cd1a6a08bddc417c96b10eb6fe215577ed20a
drperalta <peralta.135816@novaliches.sti.ph>
2018-10-22 20:33:46 +0800

    added fillable in Kit model

--- app/Kit.php
+++ app/Kit.php
@@ -2,9 +2,30 @@
 
 namespace App;
 
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 
 class Kit extends Model
 {
-    //
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'serial_number',
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'serial_number'
+    ];
 }
